ce28d572603e5740724f1ea1652b5591e5c2cc6b
Jesus Perez <jesusmpds18@gmail.com>
2022-05-09 22:33:27 -0300

    fix(index.html): changed link to my github page

--- index.js
+++ index.js
@@ -1,37 +1,35 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+if (process.env.NODE_ENV === "development") require("dotenv").config();
 const multer = require("multer");
 var app = express();
 
 // Multer setup
-const upload = multer({ dest: './public/uploads/' })
+const upload = multer({ dest: "./public/uploads/" });
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 app.post("/api/fileanalyse", upload.single("upfile"), (req, res, next) => {
-
-  if(!req.file){
-    res.json({error: "Please upload a file"})
+  if (!req.file) {
+    res.json({ error: "Please upload a file" });
   }
-  
-  const {originalname, mimetype, size} = req.file
-  
+
+  const { originalname, mimetype, size } = req.file;
+
   res.json({
     name: originalname,
     type: mimetype,
     size: size,
-  })
-  return
-})
-
+  });
+  return;
+});
 
 const port = process.env.PORT || 3000;
-app.listen(port, function() {
-  console.log('Your app is listening on port ' + port)
+app.listen(port, function () {
+  console.log("Your app is listening on port " + port);
 });
